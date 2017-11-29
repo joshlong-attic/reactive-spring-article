f20105225650fa6f312718f162ffffa00136d40b
Josh Long <starbuxman@gmail.com>
2017-11-28 18:25:08 -0800

    pdf

--- library-service/src/main/kotlin/com/example/libraryservice/SecurityConfiguration.java
+++ library-service/src/main/kotlin/com/example/libraryservice/SecurityConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
 
 @Profile("security")
 @Configuration
@@ -25,31 +27,29 @@ class SecurityConfiguration {
                 builder.username("jlong").password("pw").roles().build(),
                 builder.username("rwinch").password("pw").roles("ADMIN").build());
     }
-
-    //@formatter:off
+    
     @Bean
     @Profile("authorization")
     SecurityWebFilterChain authorization(ServerHttpSecurity http) {
-        return
-                http
-                        .httpBasic()
-                        .and()
-                        .authorizeExchange()
-                        .pathMatchers("/books/{author}").access((auth, ctx) ->
-                        auth
-                                .map(authentication -> {
-                                    Object author = ctx.getVariables().get("author");
-                                    boolean matchesAuthor = authentication.getName().equals(author);
-                                    boolean isAdmin = authentication.getAuthorities().stream()
-                                            .anyMatch(ga -> ga.getAuthority().contains("ROLE_ADMIN"));
-                                    return (matchesAuthor || isAdmin);
-                                })
-                                .map(AuthorizationDecision::new)
-                )
-                        .anyExchange().hasRole("ADMIN")
-                        .and()
-                        .build();
+        ReactiveAuthorizationManager<AuthorizationContext> am = (auth, ctx) -> auth
+            .map(authentication -> {
+                Object author = ctx.getVariables().get("author");
+                boolean matchesAuthor = authentication.getName().equals(author);
+                boolean isAdmin = authentication.getAuthorities().stream()
+                        .anyMatch(ga -> ga.getAuthority().contains("ROLE_ADMIN"));
+                return (matchesAuthor || isAdmin);
+            })
+            .map(AuthorizationDecision::new);
+        return http
+                .httpBasic()
+                .and()
+                .authorizeExchange()
+                  .pathMatchers("/books/{author}").access(am)
+                  .anyExchange().hasRole("ADMIN")
+                .and()
+                .build();
+
     }
-    //@formatter:on
+
 
 }
