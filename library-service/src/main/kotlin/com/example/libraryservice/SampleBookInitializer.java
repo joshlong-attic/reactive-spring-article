22791025dc25a2d9864cf3ac15c67bb15534ea6b
Josh Long <starbuxman@gmail.com>
2017-11-28 17:55:18 -0800

    pdf

--- library-service/src/main/kotlin/com/example/libraryservice/SampleBookInitializer.java
+++ library-service/src/main/kotlin/com/example/libraryservice/SampleBookInitializer.java
@@ -5,12 +5,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
-
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
-import java.util.function.Supplier;
 
 @Slf4j
 @Component
